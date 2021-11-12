bc6f8206b2c33beda7e11ade6038795223fc7f22
AlmasT10 <92239068+AlmasT10@users.noreply.github.com>
2021-11-12 17:24:05 -0500

    .

--- screens/AddNewTaskScreen.js
+++ screens/AddNewTaskScreen.js
@@ -1,14 +1,28 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Input, Text, Button } from "react-native-elements";
 
 const AddNewTaskScreen = () => {
   return (
-    <View>
-      <Text>Add New Task</Text>
+    <View style={styles.container}>
+      <Text></Text>
+      <Input label="Task Id" editable={false} />
+      <Input label="Task Name" placeholder="Enter Task Name" />
+      <Input label="Task Type" placeholder="Enter Task Type" />
+      <Input label="Project Name" editable={false} />
+      <Input label="Member Email Id" />
+      <Input label="Start Date" placeholder="MM/DD/YYYY" />
+      <Input label="Start Date" placeholder="MM/DD/YYYY" />
+      <Button title="Save" />
     </View>
   );
 };
 
 export default AddNewTaskScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
--- screens/EditTaskScreen.js
+++ screens/EditTaskScreen.js
@@ -1,14 +1,31 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { Input, Text, Button } from "react-native-elements";
 
 const EditTaskScreen = () => {
   return (
-    <View>
-      <Text>Edit Task Screen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text></Text>
+      <Input label="Task Id" editable={false} />
+      <Input label="Task Name" placeholder="Enter Task Name" editable={false} />
+      <Input label="Task Type" placeholder="Enter Task Type" editable={false} />
+      <Input label="Project Name" editable={false} />
+      <Input label="Member Email Id" editable={false} />
+      <Input label="Start Date" placeholder="MM/DD/YYYY" />
+      <Input label="Start Date" placeholder="MM/DD/YYYY" />
+      <Input label="Hrs Worked" keyboardType="numeric" />
+      <Input label="Comments" multiline={true} />
+
+      <Button title="Save" />
+    </ScrollView>
   );
 };
 
 export default EditTaskScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
